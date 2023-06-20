915251d9584872db0c89d24f0a06a481ffbc2509
Taushif Siddique <taushifsidd@gmail.com>
2023-06-20 17:23:09 +0530

    neww commit

---
+++ script.js
@@ -0,0 +1,11 @@
+// function abc(){
+//     let time = new Date();
+
+//     let hr= time.toLocaleTimeString();
+//     console.log(hr);
+
+//     setTimeout(()=>{
+//         abc();
+//     },1000)
+// }
+// abc();
